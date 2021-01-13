1e9fc412d98a909e3cbe6fb14a265c39cea8db75
Ricardo Baptista <riximuslol@gmail.com>
2021-01-13 16:56:42 +0100

    map as multidimensional array done

--- _Dungeon_Crawler/src/Map.java
+++ _Dungeon_Crawler/src/Map.java
@@ -1,24 +1,25 @@
 import java.lang.reflect.Array;
 
 public class Map {
-    private String[][][] map={  {{},{},{},{},{},{},{eingang},{x},{x},{x},{x},{x},{},{},{},{},{},{},{}},
-                                {{},{},{},{},{},{},{},{},{},{},{},{x},{},{},{},{},{},{},{}},
-                                {{x},{x},{x},{},{},{},{},{},{},{},{},{x},{},{},{},{},{},{},{}},
-                                {{x},{x},{x},{},{x},{x},{x},{x},{},{x},{x},{x},{},{},{},{},{},{},{}},
-                                {{x},{x},{x},{x},{bosstür},{x},{x},{x},{x},{x},{x},{x},{},{},{},{},{},{},{}},
-                                {{x},{x},{x},{},{x},{x},{x},{x},{},{x},{x},{x},{},{},{},{},{},{},{}},
 
-                                {{x},{x},{x},{},{x},{statue},{x},{x},{},{},{},{x},{},{},{},{},{},{},{}},
-                                {{},{},{},{},{},{},{},{},{},{},{},{x},{},{},{},{},{},{},{}},
-                                {{},{},{},{},{},{},{},{},{},{},{},{x},{x},{x},{x},{},{},{},{}},
-                                {{},{},{},{},{},{},{},{},{},{},{},{},{},{},{x},{x},{x},{x},{stein}},
-                                {{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{x},{x},{x}},
-                                {{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{schalter},{schalter},{schalter}}};
+    private String[][][] map={  /*0*/   {{null},{null},{null},{null},{null},{null},{"wall","6_0_1","wall","6_0_3"},{"wall","7_0_1","wand","7_0_3"},{"wall","8_0_1","wall","8_0_3"},{"wall","9_0_1","wall","9_0_3"},{"wall","10_0_1","wall","10_0_3"},{"11_0_0","corner_right","wall","11_0_3"},{null},{null},{null},{null},{null},{null},{null}},
+                                /*1*/   {{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{"11_1_0","wall","11_1_2","wall"},{null},{null},{null},{null},{null},{null},{null}},
+                                /*2*/   {{"0_2_0","0_2_1","corner_right","corner_left"},{"1_2_0","1_2_1","wall","1_2_3"},{"2_2_0","corner_right","corner_left","2_2_3"},{null},{null},{null},{null},{null},{null},{null},{null},{"11_2_0","wall","11_2_2","wall"},{null},{null},{null},{null},{null},{null},{null}},
+                                /*3*/   {{"0_3_0","0_3_1","0_3_2","wall"},{"1_3_0","1_3_1","1_3_2","1_3_3"},{"2_3_0","2_3_1","2_3_2","2_3_3"},{null},{"4_3_0","4_3_1","corner_right","4_3_3"},{"5_3_0","5_3_1","wall","5_3_3"},{"6_3_0","6_3_1","wall","6_3_3"},{"7_3_0","corner_right","corner_left","7_3_3"},{null},{"9_3_0","9_3_1","corner_right","corner_left"},{"10_3_0","10_3_1","wall","10_3_3"},{"11_3_0","wall","11_3_2","11_3_3"},{null},{null},{null},{null},{null},{null},{null}},
+                                /*4*/   {{"0_4_0","0_4_1","0_4_2","wall"},{"1_4_0","1_4_1","1_4_2","1_4_3"},{"2_4_0","2_4_1","2_4_2","2_4_3"},{"3_4_0","3_4_1","3_4_2","3_4_3"},{"4_4_0","4_4_1","4_4_2","4_4_3"},{"5_4_0","5_4_1","5_4_2","5_4_3"},{"6_4_0","6_4_1","6_4_2","6_4_3"},{"7_4_0","7_4_1","7_4_2","7_4_3"},{"wall","8_4_1","wall","8_4_3"},{"9_4_0","9_4_1","9_4_2","9_4_3"},{"10_4_0","10_4_1","10_4_2","10_4_3"},{"11_4_0","11_4_1","11_4_2","11_4_3"},{null},{null},{null},{null},{null},{null},{null}},
+                                /*5*/   {{"0_5_0","0_5_1","0_5_2","wall"},{"1_5_0","1_5_1","1_5_2","1_5_3"},{"2_5_0","2_5_1","2_5_2","2_5_3"},{null},{"4_5_0","4_5_1","4_5_2","4_5_3"},{"5_5_0","5_5_1","5_5_2","5_5_3"},{"6_5_0","6_5_1","6_5_2","6_5_3"},{"7_5_0","wall","7_5_2","7_5_3"},{null},{"corner_left","9_5_1","9_5_2","corner_right"},{"wall","10_5_1","10_5_2","10_5_3"},{"11_5_0","wall","11_5_2","11_5_3"},{null},{null},{null},{null},{null},{null},{null}},
+                                /*6*/   {{"corner_left","0_6_1","0_6_2","corner_right"},{"wall","1_6_1","1_6_2","1_6_3"},{"corner_right","corner_left","2_6_2","2_6_3"},{null},{"corner_left","4_6_1","4_6_2","corner_right"},{null},{"wall","6_6_1","6_6_2","6_6_3"},{"corner_right","corner_left","7_6_2","7_6_3"},{null},{null},{null},{"11_6_0","wall","11_6_2","wall"},{null},{null},{null},{null},{null},{null},{null}},
+                                /*7*/   {{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{"11_7_0","wall","11_7_2","wall"},{null},{null},{null},{null},{null},{null},{null}},
+                                /*8*/   {{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{"corner_left","11_8_1","11_8_2","corner_right"},{"wall","12_8_1","wall","12_8_3"},{"wall","13_8_1","wall","13_8_3"},{"14_8_0","corner_right","corner_left","14_8_3"},{null},{null},{null},{null}},
+                                /*9*/   {{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{"corner_left","14_9_1","14_9_2","corner_right"},{"wall","15_9_1","wall","15_9_3"},{"16_9_0","16_9_1","wall","16_9_3"},{"17_9_0","17_9_1","wall","17_9_3"},{null}},
+                                /*10*/  {{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{"16_10_0","16_10_1","16_10_2","wall"},{"17_10_0","17_10_1","17_10_2","17_10_3"},{"18_10_0","wall","18_10_2","18_10_3"}},
+                                /*11*/  {{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{null},{"16_11_0","16_11_1","16_11_2","wall"},{"17_11_0","17_11_1","17_11_2","17_11_3"},{"18_11_0","wall","18_11_2","18_11_3"}}
+                                };
 
     public String getImageName(int x, int y, int viewDirection){
-        String imageName = "";
-
-        return imageName;
+        String imageName = "", fileType = ".png";
+        imageName = map[y][x][viewDirection];
+        return imageName + fileType;
     }
 
     public void setMap(String[][][] mapArray){
