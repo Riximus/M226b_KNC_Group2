00cc0d221aa01fa0f596a4adf183fd3355ba2097
Silas <silas.kaegi@edu.tbz.ch>
2021-01-22 16:49:50 +0100

    started GUI

---
+++ _Dungeon_Crawler/src/GUI/MainScreen.java
@@ -0,0 +1,19 @@
+package GUI;
+
+import javax.swing.*;
+
+/**
+ * Author: Silas Kägi
+ * Projekt: Dungeon_Crawler
+ * Aufgabe:
+ * Erstellungsdatum: 22.01.2021
+ */
+public class MainScreen {
+    private JPanel jPanelPicture;
+    private JTextArea textAreaOutput;
+    private JButton eButton;
+    private JButton wButton;
+    private JButton dButton;
+    private JButton aButton;
+    private JScrollBar scrollBar1;
+}
