b0df9a4a3d06f5bd09bf340abf358fdc42c83c57
Silas <silas.kaegi@edu.tbz.ch>
2021-01-29 19:26:37 +0100

    commented main with logic tests

--- _Dungeon_Crawler/src/main.java
+++ _Dungeon_Crawler/src/main.java
@@ -5,14 +5,27 @@ import GUI.ImagePanel;
 import GUI.MainScreen;
 import GUI.TextAreaPrintStream;
 import Map.Movement;
+import Story.Story;
 
 import java.util.Scanner;
 
 public class main {
 
     public static void main(String[] args) {
-        /* Logic test of Map Movement Save
-        do {
+        /*
+        remove comments to test logics
+         */
+
+        /* Story test */
+        /*
+        Story story = new Story();
+        story.script(false); // story beginning
+        story.script(true); // story ending
+        */
+
+        /* Logic test of Map Movement */
+        /*do {
+            Fighter fighter = new Fighter();
             Movement movement = new Movement();
             Scanner scan = new Scanner(System.in);
             char input;
@@ -21,12 +34,14 @@ public class main {
             System.out.println("Your input:");
             input = scan.next().charAt(0);
 
-            System.out.println(movement.move(input));
+            System.out.println(movement.move(input, fighter));
 
             System.out.println("Walking end\n");
-        }while(true);*/
+        }while(true);
+        */
 
 
+        /* Battle test */
         /*
         Fighter fighter = new Fighter();
         Enemy enemy = new Enemy();
@@ -36,9 +51,10 @@ public class main {
         enemy.setAttackDamage(9);
         fighter.setHealth(100);
 
-        battle.startBattle(fighter, enemy);*/
+        battle.startBattle(fighter, enemy);
+        */
 
-
+        /* GUI start */
         MainScreen mainScreen = new MainScreen();
         mainScreen.run();
     }
